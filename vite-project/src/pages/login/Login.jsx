e2bffb55c0de175528d2137c5b40f52fd654c33b
Mateo Arcila <mateoarcilatoro@gmail.com>
2023-05-19 17:53:36 -0500

    Login fix

--- vite-project/src/pages/login/Login.jsx
+++ vite-project/src/pages/login/Login.jsx
@@ -1,44 +1,39 @@
-import React, { useState } from 'react'
-import HeaderPrincipal from './HeaderPrincipal'
-import Footer from '../../components/footer/Footer'
-import Registro from './Registro'
-import Inicio from './Inicio'
-import axios from 'axios'
-import { redirect } from "react-router-dom";
-
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import HeaderPrincipal from './HeaderPrincipal';
+import Footer from '../../components/footer/Footer';
+import Registro from './Registro';
+import Inicio from './Inicio';
+import axios from 'axios';
 
 const Login = () => {
   const [register, setRegistrer] = useState(true);
+  const navigate = useNavigate();
 
   const handleFormInicio = (data) => {
-    console.log(data)
+    console.log(data);
     axios.post('http://localhost:8087/estudiante/login', data).then((res) => {
-      if (res.status==200) {
-        return redirect("/home");
+      if (res.status === 200) {
+        navigate('/home');
       }
-    })
-  }
+    });
+  };
 
   const handleFormRegistro = (data) => {
-    console.log(data)
+    console.log(data);
     axios.post('http://localhost:8087/estudiante/agregar', data).then((res) => {
-      console.log(res)
-    })
-  }
+      console.log(res);
+    });
+  };
 
   return (
     <>
       <HeaderPrincipal />
-      {
-        register == false && (<Registro handleFormRegistro={handleFormRegistro} />)
-      }
-      {
-        register && (<Inicio handleFormInicio={handleFormInicio} />)
-      }
-
+      {register === false && <Registro handleFormRegistro={handleFormRegistro} />}
+      {register && <Inicio handleFormInicio={handleFormInicio} />}
       <Footer />
     </>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
