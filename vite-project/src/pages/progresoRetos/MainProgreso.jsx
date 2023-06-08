37ca22cf39b4fcda781a3282aaf72fc85937b6b7
yorman-129 <yorman_tobon82201@elpoli.edu.co>
2023-06-07 22:22:25 -0500

    Feat: descarga del progreso de los retos

--- vite-project/src/pages/progresoRetos/MainProgreso.jsx
+++ vite-project/src/pages/progresoRetos/MainProgreso.jsx
@@ -1,23 +1,14 @@
 import React, { useEffect, useState } from 'react';
-<<<<<<< HEAD
-
-const MainProgreso = ({ data }) => {
-  const [dataRetos, setDataRetos] = useState(null);
-
-=======
 import axios from 'axios';
 
 const MainProgreso = ({ data }) => {
   const [dataRetos, setDataRetos] = useState(null);
   
->>>>>>> 59dd180704ac159e1ca90302c6ac167ecbc61504
   useEffect(() => {
     setDataRetos(data);
   }, [data]);
 
-<<<<<<< HEAD
-=======
-  const handleButton=async(id)=>{
+  const handleButton = async(id)=>{
     try {
       const response = await axios.get(`http://localhost:8087/retos/${id}/solucion`, {
         responseType: 'blob',
@@ -36,22 +27,17 @@ const MainProgreso = ({ data }) => {
     }
   }
 
->>>>>>> 59dd180704ac159e1ca90302c6ac167ecbc61504
   return (
     <>
       <ul>
         {dataRetos &&
           dataRetos.map((reto, index) => (
-<<<<<<< HEAD
-            <li key={index}>{reto.id}</li>
-=======
             <li key={index}>
               <div>{reto.id}</div>
               <div>{reto.empresa.nombre}</div>
               <div>{reto.estado ? 'activo':'inactivo'}</div>
-              <div><button onClick={handleButton(reto.id)}>Descargar Solucion</button></div>
+              <div><button  onClick={()=>handleButton(reto.id)}>Descargar Solucion</button></div>
               </li>
->>>>>>> 59dd180704ac159e1ca90302c6ac167ecbc61504
           ))}
       </ul>
     </>
